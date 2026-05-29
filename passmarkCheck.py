66dfb38e704305a56f819d04509fd6bdf11fa565
naheemah-babs <babatundenaheemot@gmail.com>
2026-05-29 09:33:54 +0100

    passmark check project

---
+++ passmarkCheck.py
@@ -0,0 +1,9 @@
+passmark = 20
+
+if passmark == 20:
+    print("You passed")  
+elif score <= 12:
+    print("You need to take a retest") 
+else:
+    print("You failed and you will need to come back")    
+
