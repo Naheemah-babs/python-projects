80e92a889069fca9ef70241027a5bfc6259ff4fe
naheemah-babs <babatundenaheemot@gmail.com>
2026-06-24 09:00:14 +0100

    implement file word counter function

--- fileHandling3.py
+++ fileHandling3.py
@@ -5,3 +5,18 @@ def copy_file(filename):
 
 copy_file("sample.txt")
 
+#counting number of occurence of a specific word in a text file
+specWord = input("enter a word: \n")
+def count_word(filename):
+    with open ("sample.txt", "r") as file:
+        lines = file.readlines()
+        word_count = 0
+        for line in lines:
+            words = line.split()
+
+            for word in words:
+                if word == specWord:
+                    word_count += 1
+    print(f"'{specWord}' appears {word_count} times.")      
+        
+count_word("sample.txt")
