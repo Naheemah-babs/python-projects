1340e6fb4dfb78daf716bad6ea87da9cfb9d7498
naheemah-babs <babatundenaheemot@gmail.com>
2026-07-12 04:14:01 +0100

    dataman.py

---
+++ dataman.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+data = {
+    "Name": ["Alice", "Bob", np.nan, "David"],
+    "Age": [25, np.nan, 30, 35]
+    "Score": [85, 90, np.nan. 88],
+}
+df = pd.DataFrame(data)
+print("Original Dataset: \n", df)
+
+df["Age"] = df["Age"].fillna(df["Age"].mean())
+df["Score"] = df["Score"].interpolate()
+
+df= df.rename(columns={"Name": "Student_Name", "Score": "Student_Score"})
+print("Original Dataset: \n", df)
